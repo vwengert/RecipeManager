17c03c04e32f0b7e6c5a6654d836e70be42ba52c
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2022-05-21 22:01:57 +0200

    added Service and Tests

--- src/test/java/de/kochen/food/controller/FoodControllerTest.java
+++ src/test/java/de/kochen/food/controller/FoodControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
