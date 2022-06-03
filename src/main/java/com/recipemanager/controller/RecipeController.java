4e638704d57c47f502b40218dc6ed2e4de548b1d
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2022-06-03 07:34:26 +0200

    RecipeById Controller

--- src/main/java/com/recipemanager/controller/RecipeController.java
+++ src/main/java/com/recipemanager/controller/RecipeController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/v1/")
 public class RecipeController {
 
-	@GetMapping(path = "recipe")
-	public ResponseEntity<String> getRecipe() {
-		return new ResponseEntity<>("", HttpStatus.OK);
+	@GetMapping(path = "recipeById/{recipeId}")
+	public ResponseEntity<String> getRecipe(@PathVariable Long recipeId) {
+		return new ResponseEntity<>("[{\"recipeHeaderName\":\"Suppe\"," +
+				"\"foodName\":\"Kartoffel\"," +
+				"\"Quantity\":3}]", HttpStatus.OK);
 	}
 
 }
