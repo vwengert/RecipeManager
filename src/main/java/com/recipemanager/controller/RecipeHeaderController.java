27e6b1329b0b5b59d8644dfb83d18661b40145ee
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2022-06-03 07:28:00 +0200

    RecipeController hinzu

--- src/main/java/com/recipemanager/controller/RecipeHeaderController.java
+++ src/main/java/com/recipemanager/controller/RecipeHeaderController.java
@@ -21,36 +21,36 @@ public class RecipeHeaderController {
 	final private RecipeHeaderService recipeHeaderService;
 
 	@GetMapping(path = "recipeHeader")
-	public ResponseEntity<List<RecipeHeader>> getRecipe() {
+	public ResponseEntity<List<RecipeHeader>> getRecipeHeader() {
 		return new ResponseEntity<>(recipeHeaderService.getRecipeHeader(), HttpStatus.OK);
 	}
 
 	@GetMapping(path = "recipeHeaderById/{recipeId}")
-	public ResponseEntity<RecipeHeader> getRecipeById(@PathVariable Long recipeId) throws NotFoundException {
+	public ResponseEntity<RecipeHeader> getRecipeHeaderById(@PathVariable Long recipeId) throws NotFoundException {
 		return new ResponseEntity<>(recipeHeaderService.getRecipeHeaderById(recipeId), HttpStatus.OK);
 	}
 
 	@GetMapping(path = "recipeHeaderByName/{recipeName}")
-	public ResponseEntity<RecipeHeader> getRecipeByName(@PathVariable String recipeName) throws NotFoundException {
+	public ResponseEntity<RecipeHeader> getRecipeHeaderByName(@PathVariable String recipeName) throws NotFoundException {
 		return new ResponseEntity<>(recipeHeaderService.getRecipeHeaderByName(recipeName), HttpStatus.OK);
 	}
 
 	@PostMapping(path = "recipeHeader",
 			consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<RecipeHeader> postRecipe(@RequestBody RecipeHeader recipeHeader) throws IdNotAllowedException, FoundException {
+	public ResponseEntity<RecipeHeader> postRecipeHeader(@RequestBody RecipeHeader recipeHeader) throws IdNotAllowedException, FoundException {
 		return new ResponseEntity<>(recipeHeaderService.postRecipeHeader(recipeHeader), HttpStatus.CREATED);
 	}
 
 	@PutMapping(path = "recipeHeader",
 			consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<RecipeHeader> putRecipe(@RequestBody RecipeHeader recipeHeader) throws NotFoundException {
+	public ResponseEntity<RecipeHeader> putRecipeHeader(@RequestBody RecipeHeader recipeHeader) throws NotFoundException {
 		return new ResponseEntity<>(recipeHeaderService.putRecipeHeader(recipeHeader), HttpStatus.OK);
 	}
 
 	@DeleteMapping(path = "recipeHeader/{recipeId}")
-	public ResponseEntity<?> deleteRecipe(@PathVariable Long recipeId) throws NoContentException {
+	public ResponseEntity<?> deleteRecipeHeader(@PathVariable Long recipeId) throws NoContentException {
 		recipeHeaderService.deleteRecipeHeader(recipeId);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
---
+++ src/test/java/com/recipemanager/controller/RecipeControllerIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.recipemanager.controller;
+
+import com.recipemanager.RecipeManagerApplication;
+import com.recipemanager.model.RecipeHeader;
+import com.recipemanager.repository.RecipeHeaderRepository;
+import com.recipemanager.util.annotations.IntegrationTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = RecipeManagerApplication.class)
+@AutoConfigureMockMvc
+class RecipeControllerIntegrationTest {
+	@Autowired
+	MockMvc mockMvc;
+
+	@BeforeEach
+	@Transactional
+	void setUp() {
+	}
+
+
+	@IntegrationTest
+	@Transactional
+	void getRecipeById_Returns200() throws Exception {
+
+		mockMvc.perform(get("/api/v1/recipeById/1").contentType("application/json"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].recipeHeaderName").value("Suppe"))
+				.andExpect(jsonPath("$[0].foodName").value("Kartoffel"))
+				.andExpect(jsonPath("$[0].Quantity").value(3));
+	}
+
+
+
+}
